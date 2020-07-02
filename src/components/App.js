3fa2f85bd6a30a190626f611403532bf611c8a72
Preston Carroll <plutusinc1@yahoo.com>
2020-07-01 19:04:31 -0500

    on change state set, controlled component, and search bar set

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import SearchBar from './SearchBar'
 
 
 
 const App = () => {
-    return <div> App
+    return <div className="ui container" style= {{ marginTop: '10px' }}> <SearchBar />
 
- </div>;
+    </div>;
 };
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
 
+class SearchBar extends React.Component {
 
+    state = { term: '' };
+
+    // onInputClick() {
+    //     console.log("input was clicked")
+    // }
+
+    render() {
+        return (<div className="ui segment">
+
+            <form className="ui form">
+                <div className="field">
+                    <label> Image Search</label>
+                    <input type="text"
+                        value={this.state.term}
+                        onChange={e => this.setState({ term: e.target.value })}
+                    // onClick={this.onInputClick}
+                    />
+                </div>
+            </form>
+        </div>
+        );
+    }
+}
 
 
 export default SearchBar;
