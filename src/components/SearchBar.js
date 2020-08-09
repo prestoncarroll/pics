fb7e352605300e7574bfa8499daefe17557f8292
Preston Carroll <plutusinc1@yahoo.com>
2020-08-09 11:20:57 -0500

    prevented default

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,15 +9,28 @@ class SearchBar extends React.Component {
     //     console.log("input was clicked")
     // }
 
+    //semantic ui for the styling
+
+
+    //onformsubmit prevents the page from refreshing every search
+    onFormSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(this.state.term);
+    }
+
     render() {
         return (<div className="ui segment">
 
-            <form className="ui form">
+            <form onSubmit={(e) => this.onFormSubmit(e)} className="ui form">
                 <div className="field">
                     <label> Image Search</label>
                     <input type="text"
                         value={this.state.term}
                         onChange={e => this.setState({ term: e.target.value })}
+
+
+                    //console.log(state)
                     // onClick={this.onInputClick}
                     />
                 </div>
