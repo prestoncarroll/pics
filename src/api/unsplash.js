4b96b44ab3b6eabf1b463b5e9ed650d972b8192a
Preston Carroll <plutusinc1@yahoo.com>
2020-08-15 14:59:30 -0500

    height and rows set to images with grid rows

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -7,6 +7,7 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
         Authorization: 'Client-ID BabsQgH_BEJa_4-h2ksTQ8MPjP3lWok4TeBfbT5pxOU'
+        
     }
 
 });
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,18 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
+
 
 const ImageList = (props) => {
 
-    const images = props.images.map(({description, id, urls}) => {
-        return <div key={id}>
-            <img alt={description} src={urls.regular} />
+    const images = props.images.map((image) => {
+        return <div key={image.id}>
+            <ImageCard image={image}/>
         </div>
     });
 
     // console.log(props.images);
-    return <div>
+    return <div className="image-list">
         {images}
     </div>;
 };
