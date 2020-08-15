24f76cce60e853640bf717dada1703679f9cd3fa
Preston Carroll <plutusinc1@yahoo.com>
2020-08-15 08:26:12 -0500

    images now appear on the page

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
+const ImageList = (props) => {
 
+    const images = props.images.map(({description, id, urls}) => {
+        return <div key={id}>
+            <img alt={description} src={urls.regular} />
+        </div>
+    });
+
+    // console.log(props.images);
+    return <div>
+        {images}
+    </div>;
+};
 
 
 
